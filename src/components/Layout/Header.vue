4383d787bb383aaa999dd071ef4e537966703a9f
Ekaterina Gibova <katyagibova@bk.ru>
2022-02-24 21:16:45 +0300

    header added

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-container>
+    <v-toolbar flat>
+      <div  sm="2" md="1" class="mr-4 fix-margin-left">
+        <router-link to="/" class="reset-link">
+          <div class="my-logo"></div>
+        </router-link>
+      </div>
+      <v-toolbar-title>
+        <div class="logo-text">Aced deal!</div>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <div class="d-none d-md-flex">
+        <!-- <v-text-field
+          label="Поиск"
+          single-line
+          prepend-inner-icon="mdi-magnify"
+          outlined
+          dense
+          hide-details="auto"
+          class="rounded-lg"
+        ></v-text-field> -->
+
+        <v-btn
+          class="ml-4 rounded-lg fix-margin-right"
+          height="40px"
+          depressed
+          dark
+          color="#3B7978"
+          @click="$router.push({ name: 'user-profile', params: { userId } })"
+        >
+          Личный кабинет
+        </v-btn>
+
+        <!-- <v-btn
+          class="ml-7 rounded-lg fix-margin-right"
+          height="40px"
+          dark
+          depressed
+          color="#3B7978"
+          @click="logout"
+        >
+          Выйти
+        </v-btn>         -->
+      </div>
+    </v-toolbar>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: "Header",
+  data() {
+    return {
+    }
+  },
+  computed: {},
+  created(){},
+  methods: {},
+};
+</script>
+
+<style>
+
+.logo-text{
+    font-family: 'ABeeZee', sans-serif;
+    font-size: 36px;
+    line-height: 37px;
+}
+
+.fix-margin-left {
+  margin-left: -16px;
+}
+
+.fix-margin-right {
+  margin-right: -16px;
+}
+.v-badge__badge {
+  color: black !important;
+}
+.my-logo {
+  width: 40px;
+  height: 40px;
+  background-color: #3B7978;
+  border-radius: 8px;
+}
+</style>
