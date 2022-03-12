32bb18ffabddedc525bf5eae6bf756a8c1845366
Ekaterina Gibova <katyagibova@bk.ru>
2022-03-12 23:08:53 +0300

    added percentage calculation

--- src/views/Admin/UserProfile.vue
+++ src/views/Admin/UserProfile.vue
@@ -49,7 +49,8 @@
             flat
             ></v-textarea>
         </div>
-        <div class="d-flex">
+        <v-row>
+          <v-col cols="4">
             <div class="mr-2">
                 Сумма займа
                 <v-text-field
@@ -60,8 +61,20 @@
                 background-color="#F5F5F5">
                 </v-text-field>
             </div>
+          </v-col>
+          <v-col cols="8">
+            <div>
+              <div>
+                Итоговая процентная ставка:
+              </div>
+              <div class="percent d-flex flex-coloumn align-center justify-center">{{percent}}%</div>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
             <div class="mr-2">
-                Срок выплаты (в месяцах)
+              Срок выплаты (в месяцах)
                 <v-text-field
                 v-model="monthsNumber"
                 hide-details="auto"
@@ -70,35 +83,44 @@
                 background-color="#F5F5F5">
                 </v-text-field>
             </div>
+          </v-col>
+          <v-col cols="8">
             <div>
-                <div>
-                    Итоговая процентная ставка:
-                </div>
-                <div style="margin-top: auto">3% в месяц</div>
-                
+              <div>
+                Ежемесячный платёж:
+              </div>
+              <div class="percent d-flex flex-coloumn align-center justify-center">3000₽</div>
             </div>
-        </div>
-        <div>
+          </v-col>
+        </v-row>
+        <!-- <div>
             <v-slider
             color="#3B7978"
             track-color="#3B7978"
             v-model="slider"
             thumb-label="always"
             ></v-slider>
-        </div>
-        <div>
+        </div> -->
+        <v-row>
+          <v-col cols="2">
             <v-btn
             color="#3B7978"
             dark
+            block
             depressed
             class="mr-2"
             @click="postNewApplication">Сохранить</v-btn>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="2">
             <v-btn
+            block
             color="#F5F5F5"
             depressed>
-                Отмена
+              Отмена
             </v-btn>
-        </div>
+          </v-col>
+        </v-row>
     </v-card>
   </v-dialog>
 </div>
@@ -114,37 +136,50 @@ export default {
   computed: {
     ...mapState(["user_applications"]),
     generateId(){
-        const id = '_' + Math.random().toString(36).substr(2, 9);
-        return id
+      const id = '_' + Math.random().toString(36).substr(2, 9);
+      return id
     }
   },
   data(){
     return {
-        title: "",
-        description: "",
-        sum: null,
-        monthsNumber: null,
-        create_application_dialog: false,
+      title: "",
+      description: "",
+      sum: null,
+      monthsNumber: null,
+      percent: 0,
+      create_application_dialog: false,
+    }
+  },
+  watch: {
+    sum(value){
+      if(value == 0){
+        this.percent = 0
+      } else if(value > 0 && value < 10000){
+        this.percent = 3
+      } else if(value >= 10000 && value < 30000){
+        this.percent = 4
+      } else {
+        const number = value / 30000
+        this.percent = 4 + Math.trunc(number)
+      }
     }
   },
   methods: {
     postNewApplication(){
-        const application = {
-            id: this.generateId,
-            title: this.title,
-            sum: Number(this.sum),
-            description: this.description,
-            percent: 5,
-            publicationDate: new Date,
-            monthsNumber: Number(this.monthsNumber),
-            person: "Гибова Екатерина",
-            personId: 4
-        }
-        console.log(application)
+      const application = {
+        id: this.generateId,
+        title: this.title,
+        sum: Number(this.sum),
+        description: this.description,
+        percent: 5,
+        publicationDate: new Date,
+        monthsNumber: Number(this.monthsNumber),
+        person: "Гибова Екатерина",
+        personId: 4
+      }
+      console.log(application)
         // this.$store.dispatch("postNewApplication", application)
     },
-
-    
   },
   created(){
     this.$store.dispatch("getUserApplication")    
@@ -153,4 +188,9 @@ export default {
 </script>
 
 <style>
+.percent{
+  font-size: 32px;
+  line-height: 40px;
+  color: #3B7978;
+}
 </style>
