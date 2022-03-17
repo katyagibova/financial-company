f7d71b2e0a5c1f893ec571430ae179ea092b59f8
Ekaterina Gibova <katyagibova@bk.ru>
2022-03-17 03:53:02 +0300

    adapted application catalog

--- src/components/Applications/ApplicationCatalog.vue
+++ src/components/Applications/ApplicationCatalog.vue
@@ -41,7 +41,7 @@
       </v-col>
     </v-row> -->
     <v-row dense class="my-2">
-      <v-col cols="4">
+      <v-col cols="5" lg="4" sm="4" class="d-none d-md-flex align-start">
         <v-card elevation="0" class="rounded-xl pa-3">
           <header class="filter_header">
             Статус заявки
@@ -63,55 +63,63 @@
           <header class="filter_header">
             Сумма сделки
           </header>
-          <div class="d-flex mb-5">
-            <v-text-field
-            color="#3B7978"
-            v-model="fromSum"
-            placeholder="От"
-            hide-details="auto"
-            class="rounded-lg"
-            dense
-            elevation="0"
-            outlined
-            ></v-text-field>
+          <v-row class="mb-2">
+            <v-col cols="12" md="6">
+              <v-text-field
+              color="#3B7978"
+              v-model="fromSum"
+              placeholder="От"
+              hide-details="auto"
+              class="rounded-lg"
+              dense
+              elevation="0"
+              outlined
+              ></v-text-field>
+            </v-col>
             <v-spacer></v-spacer>
-            <v-text-field
-            color="#3B7978"
-            v-model="toSum"
-            placeholder="До"
-            hide-details="auto"
-            class="rounded-lg"
-            dense
-            elevation="0"
-            outlined
-            ></v-text-field>
-          </div>
+            <v-col cols="12" md="6">
+              <v-text-field
+              color="#3B7978"
+              v-model="toSum"
+              placeholder="До"
+              hide-details="auto"
+              class="rounded-lg"
+              dense
+              elevation="0"
+              outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
           <header class="filter_header">
             Доход со сделки
           </header>
-          <div class="d-flex mb-5">
-            <v-text-field
-            color="#3B7978"
-            v-model="fromRevenue"
-            placeholder="От"
-            hide-details="auto"
-            class="rounded-lg"
-            dense
-            elevation="0"
-            outlined
-            ></v-text-field>
+          <v-row class="mb-2">
+            <v-col cols="12" md="6">
+              <v-text-field
+              color="#3B7978"
+              v-model="fromRevenue"
+              placeholder="От"
+              hide-details="auto"
+              class="rounded-lg"
+              dense
+              elevation="0"
+              outlined
+              ></v-text-field>
+            </v-col>
             <v-spacer></v-spacer>
-            <v-text-field
-            color="#3B7978"
-            v-model="toRevenue"
-            placeholder="До"
-            hide-details="auto"
-            class="rounded-lg"
-            dense
-            elevation="0"
-            outlined
-            ></v-text-field>
-          </div>
+            <v-col cols="12" md="6">
+              <v-text-field
+              color="#3B7978"
+              v-model="toRevenue"
+              placeholder="До"
+              hide-details="auto"
+              class="rounded-lg"
+              dense
+              elevation="0"
+              outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
           <v-card-actions class="pa-0">
             <v-btn
             @click="fetchApplication"
@@ -124,15 +132,129 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col
-      cols="8"
-      >
+      <v-col lg="8" xs="12">
         <v-row>
-          <v-col cols="4">
+          <v-col cols="2" class="d-md-none" sm="1">
+            <v-btn @click="drawer = !drawer" height="100%" depressed color="#fff" small class="rounded-lg">
+              <v-icon color="#000" size="28">
+                mdi-tune-variant
+              </v-icon>
+            </v-btn>
+            <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            width="100%"
+            temporary
+            >
+              <v-card elevation="0" class="rounded-xl pa-3">
+                <div class="d-flex align-center">
+                  <div class="filter_header text-decoration-underline mb-0">Фильтры</div>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                  @click="drawer = false"
+                  depressed
+                  icon>
+                    <v-icon>
+                      mdi-close
+                    </v-icon>
+                  </v-btn>
+                </div>
+                <header class="filter_header">
+                  Статус заявки
+                </header>
+                <div class="d-flex mb-5">
+                  <v-radio-group
+                  class="mt-0 pt-0"
+                  hide-details="auto" 
+                  v-model="radioGroup">
+                    <v-radio
+                    color="#3B7978"
+                    v-for="item in status"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                    ></v-radio>
+                  </v-radio-group>
+                </div>
+                <header class="filter_header">
+                  Сумма сделки
+                </header>
+                <v-row class="mb-5">
+                  <v-col cols="6">
+                    <v-text-field
+                    color="#3B7978"
+                    v-model="fromSum"
+                    placeholder="От"
+                    hide-details="auto"
+                    class="rounded-lg"
+                    dense
+                    elevation="0"
+                    outlined
+                    ></v-text-field>
+                  </v-col>
+                  <v-spacer></v-spacer>
+                  <v-col cols="6">
+                    <v-text-field
+                    color="#3B7978"
+                    v-model="toSum"
+                    placeholder="До"
+                    hide-details="auto"
+                    class="rounded-lg"
+                    dense
+                    elevation="0"
+                    outlined
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <header class="filter_header">
+                  Доход со сделки
+                </header>
+                <v-row class="mb-5">
+                  <v-col cols="6">
+                    <v-text-field
+                    color="#3B7978"
+                    v-model="fromRevenue"
+                    placeholder="От"
+                    hide-details="auto"
+                    class="rounded-lg"
+                    dense
+                    elevation="0"
+                    outlined
+                    ></v-text-field>
+                  </v-col>
+                  <v-spacer></v-spacer>
+                  <v-col cols="6">
+                    <v-text-field
+                    color="#3B7978"
+                    v-model="toRevenue"
+                    placeholder="До"
+                    hide-details="auto"
+                    class="rounded-lg"
+                    dense
+                    elevation="0"
+                    outlined
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-card-actions class="pa-0">
+                  <v-btn
+                  @click="fetchApplication"
+                  class="rounded-lg"
+                  color="#3B7978"
+                  dark
+                  depressed>
+                    Отфильтровать
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-navigation-drawer>
+          </v-col>
+          <v-col cols="10" lg="4" sm="5">
             <v-select
             @change="fetchApplication"
             :items="sortBy"
             v-model="sortValue"
+            class="rounded-lg"
             return-object
             item-color="teal darken-3"
             color="teal darken-3"
@@ -143,8 +265,13 @@
             hide-details="auto"
             ></v-select>
           </v-col>
-          <v-col align-self="center">
-            <div >
+          <v-col class="d-none d-sm-flex" align-self="center">
+            <div>
+              Найдено результатов: {{applications.length}}
+            </div>
+          </v-col>
+          <v-col class="d-sm-none pt-0" align-self="center">
+            <div>
               Найдено результатов: {{applications.length}}
             </div>
           </v-col>
@@ -201,6 +328,8 @@ export default {
       toRevenue: 0,
       radioGroup: "Опубликовано",
       status:[ "Опубликовано", "Одобрено", "Завершено" ],
+      drawer: false,
+      group: null,
     }
   },
   methods: {
@@ -216,6 +345,7 @@ export default {
       this.$store.dispatch("getApp", filters).then(() => {
         this.toRevenue = this.maxRevenue
         this.toSum = this.maxSum
+        this.drawer = false
       })
     },
   },
